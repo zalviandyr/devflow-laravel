a32a33eb03e99f3dd67247c788a2e2c600abb539
zalviandyr <zukronalviandy@gmail.com>
2022-10-23 04:05:36 +0700

    feat: media library permission

--- config/filesystems.php
+++ config/filesystems.php
@@ -60,6 +60,16 @@ return [
             'driver' => 'local',
             'root' => public_path('media'),
             'url' => env('APP_URL').'/media',
+            'permissions' => [
+                'file' => [
+                    'public' => 0774,
+                    'private' => 0600,
+                ],
+                'dir' => [
+                    'public' => 0775,
+                    'private' => 0700,
+                ],
+            ],
         ],
     ],
 
