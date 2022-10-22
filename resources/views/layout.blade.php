2dd27044aeae103b82585388ebd39dc754fc6abb
zalviandyr <zukronalviandy@gmail.com>
2022-10-23 00:00:17 +0700

    feat: user name in sidebar

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -61,7 +61,7 @@
       <div class="sticky w-full top-20">
         @auth
           <img src="{{ asset('images/avatar.svg') }}" class="w-32 h-32 mx-auto rounded-full">
-          <div class="mt-4 text-center">Alvin Faiz</div>
+          <div class="mt-4 text-center">{{ Auth::user()->name }}</div>
           <div class="my-10 text-center hover:cursor-pointer" @click="createPost = !createPost">
             <div class="w-full py-4 rounded-full" :class="createPost ? 'border border-red-500 text-red-500' : 'bg-red-500 text-white'">
               Create Post
