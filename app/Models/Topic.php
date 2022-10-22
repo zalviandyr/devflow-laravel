9cf9f509a09ffea72a100806533df85263b30da9
zalviandyr <zukronalviandy@gmail.com>
2022-10-23 03:16:18 +0700

    feat: topic category

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -17,4 +17,13 @@ class Topic extends Model
         'category_id',
     ];
 
+    protected $with = [
+        'category',
+    ];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
 }
