184c041ee66543b09c5e8afcff379559d1686157
zalviandyr <zukronalviandy@gmail.com>
2022-10-23 06:18:55 +0700

    feat: send email

--- app/Http/Livewire/Comment.php
+++ app/Http/Livewire/Comment.php
@@ -3,6 +3,10 @@
 namespace App\Http\Livewire;
 
 use App\Models\Comment as Komen;
+use App\Models\Post;
+use App\Services\EmailService;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Mail;
 use Livewire\Component;
 
 class Comment extends Component
@@ -11,23 +15,33 @@ class Comment extends Component
     protected $rules = [
         'body' => 'required',
     ];
- 
+
     public $comment = '';
     public $mid;
+    public $loading = true;
 
     public function mount($post)
     {
         $this->mid = $post->id;
     }
 
-    public function addComment(){
-        // $post_id = $this->post->id;
+    public function addComment()
+    {
+        $this->loading = false;
+
+        $auth = Auth::user();
+
+        // send email
+        $post = Post::find($this->mid);
+        Mail::to($post->user->email)->send(new EmailService($post->title, $this->comment, $auth->email, $auth->name));
+
         Komen::create([
-            'user_id' => auth()->user()->id,
+            'user_id' => $auth->id,
             'post_id' => $this->mid,
             'body' => $this->comment,
         ]);
-        $this->comment ='';
+        $this->comment = '';
+        $this->loading = true;
         $this->render();
     }
 
@@ -35,7 +49,7 @@ class Comment extends Component
     {
         $komen = Komen::where('post_id', $this->mid)->latest()->get();
         return view('livewire.comment', [
-            'comments' => $komen
+            'comments' => $komen,
         ]);
     }
 }
