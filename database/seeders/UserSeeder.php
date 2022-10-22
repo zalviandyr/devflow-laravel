4f36c97b226cc29c3afe63a3aa2ad02c2db2bbb0
Alvin Faiz <softkopi3258@gmail.com>
2022-10-23 03:02:32 +0700

    fix seed user

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
@@ -18,8 +19,15 @@ class UserSeeder extends Seeder
             'name' => fake()->name,
             'username' => fake()->userName,
             'email' => fake()->email,
-            'password' => fake()->password,
-            'phone_number' => '089696133160',
+            'password' => Hash::make('12345678'),
+            'address' => fake()->address,
+            'is_Admin' => true
+        ]);
+        User::create([
+            'name' => fake()->name,
+            'username' => fake()->userName,
+            'email' => fake()->email,
+            'password' => Hash::make('12345678'),
             'address' => fake()->address,
         ]);
     }
